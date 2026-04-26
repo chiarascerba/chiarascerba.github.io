ecf17ced959ed60f9f3c89fdca4294f1cb8fc705
alberto <alby5062@gmail.com>
2026-04-26 14:27:20 +0200

    fix: add support for fallback Sanity project configuration via secrets and default values

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -5,8 +5,8 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'u71i3zyz',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-26',
   useCdn: false,
 });
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { schemaTypes } from './sanity/schemas';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'u71i3zyz';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 export default defineConfig({
   name: 'default',
